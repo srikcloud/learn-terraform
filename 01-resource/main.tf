676cfbf6bc19bca39e1c7ebe4f2516ba8086ccf4
srikanth152113 <srikanth152113@gmail.com>
2025-12-30 13:37:09 +0530

    updated

---
+++ 01-resource/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+     features {}
+}
+
+resource "azurerm_virtual_machine" "test" {
+  name                  = "test-vm"
+  location              = "UK West"
+  resource_group_name   = "rg-roboshop"
+  network_interface_ids = ["/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
+  vm_size               = "Standard_B2als v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+   id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Compute/images/local-devops-practice"
+  }
+  storage_os_disk {
+    name              = "test-vm-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "test-vm"
+    admin_username = "azureuser"
+    admin_password = "DevOps@123456"
+  }
+}
