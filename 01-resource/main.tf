81ec2094b3877ece3d49734e015c17c81a212c17
srikanth152113 <srikanth152113@gmail.com>
2025-12-30 13:57:21 +0530

    updated

--- 01-resource/main.tf
+++ 01-resource/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_machine" "test" {
   location              = "UK West"
   resource_group_name   = "rg-roboshop"
   network_interface_ids = ["/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
-  vm_size               = "Standard_B2als v2"
+  vm_size               = "Standard_B2als_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   delete_os_disk_on_termination = true
