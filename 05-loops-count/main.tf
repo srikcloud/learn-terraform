502116241943a425db8ce7f8ac7cb73bec6f8f20
srikanth152113 <srikanth152113@gmail.com>
2026-01-05 11:18:00 +0530

    updated

--- 05-loops-count/main.tf
+++ 05-loops-count/main.tf
@@ -2,6 +2,9 @@ resource "null_resource" "test" {
     count = 10
 }
 
+# count resource attributes are referred with resourcelabel.locallabel[*].attribute 
+#(* denoting all values, if we want first value then [0])
+
 output "test" {
   value = null_resource.test[*].id
 }
@@ -11,4 +14,57 @@ resource "null_resource" "testx" {
 
 output "testx" {
   value = null_resource.testx.id
+}
+
+## count is not a great one to start for looping, as it has its own problems
+
+variable "nodes" {
+  default = [
+     "test1",
+     "test2"
+  ]
+}
+
+resource "azurerm_network_interface" "privateip" {
+  count               = length(var.nodes)
+  name                = "${var.nodes[count.index]}-ip"
+  location            = "UK West"
+  resource_group_name = "rg-roboshop"
+
+  ip_configuration {
+    name                          = "${var.nodes[count.index]}-ip"
+    subnet_id                     = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/virtualNetworks/roboshop-network/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "test" {
+  count                 = length(var.nodes)
+  name                  = "${var.nodes[count.index]}-vm"
+  location              = "UK West"
+  resource_group_name   = "rg-roboshop"
+  network_interface_ids = ["/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/networkInterfaces/terraform-testing"]
+  vm_size               = "Standard_B2als_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+   id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Compute/images/local-devops-practice"
+  }
+  storage_os_disk {
+    name              = "${var.nodes[count.index]}-vm-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "${var.nodes[count.index]}-vm"
+    admin_username = "azureuser"
+    admin_password = "DevOps@123456"
+  }
+ os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
 }
