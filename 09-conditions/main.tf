58a74781968fc8bcbff3f77bb7aeb25c9f8c7fc1
srikanth152113 <srikanth152113@gmail.com>
2026-01-05 12:08:44 +0530

    updated

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 variable "nodes" {
   default = {
       test2 = {
-        private_ip_address_allocation = "Dynamic"
+        private_ip_address_allocation = "Static"
      }
       test1 = {
       }
@@ -22,6 +22,9 @@ resource "azurerm_network_interface" "privateip" {
   ip_configuration {
     name                          = "${each.key}-ip"
     subnet_id                     = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/virtualNetworks/roboshop-network/subnets/default"
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = length(each.value["private_ip_address_allocation"]) > 0 ? each.value["private_ip_address_allocation"] : "Dynamic"
   }
 }
+
+# condition ? true_val ? false_val
+# condition in terraform are meant to pick up a value, not meant to run a resource or not
