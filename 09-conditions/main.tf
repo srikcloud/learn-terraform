c4f4b39a9fc277947558825f8f34c30de71799b8
srikanth152113 <srikanth152113@gmail.com>
2026-01-05 12:02:57 +0530

    updated

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -1,7 +1,27 @@
-resource "null_resource" "test" {
-  
+provider "azurerm" {
+     features {}
+     subscription_id = "f7fcf972-1a3d-456c-a347-914d12f8c308"
 }
 
-output "test" {
-  value = length(null_resource.test)
+variable "nodes" {
+  default = {
+      test2 = {
+        private_ip_address_allocation = "Dynamic"
+     }
+      test1 = {
+      }
+   }
+}
+
+resource "azurerm_network_interface" "privateip" {
+  for_each            = var.nodes
+  name                = "${each.key}-ip"
+  location            = "UK West"
+  resource_group_name = "rg-roboshop"
+
+  ip_configuration {
+    name                          = "${each.key}-ip"
+    subnet_id                     = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/virtualNetworks/roboshop-network/subnets/default"
+    private_ip_address_allocation = "Dynamic"
+  }
 }
