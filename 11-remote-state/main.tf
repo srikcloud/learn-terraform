22c6212cdd34a394435ce63bea8f79ff781ecffe
srikanth152113 <srikanth152113@gmail.com>
2026-01-09 23:15:44 +0530

    updated

---
+++ 11-remote-state/main.tf
@@ -0,0 +1,15 @@
+resource "null_resource" "test" {
+  
+}
+
+terraform {
+  backend "azurerm" {
+    use_cli              = true                                    
+    # tenant_id            = "00000000-0000-0000-0000-000000000000"  
+    subscription_id      = "f7fcf972-1a3d-456c-a347-914d12f8c308"  
+    resource_group_name  = "rg-roboshop"          
+    storage_account_name = "roboshopdevtfstate"                              
+    container_name       = "roboshop-state-file"                               
+    key                  = "test.terraform.tfstate"                
+  }
+}
